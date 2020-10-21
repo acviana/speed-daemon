8695bb9089dd8f4e97f2d783c43eff28b71ab5ef
Alex C. Viana <alexcostaviana@gmail.com>
2020-10-20 20:45:09 -0500

    Added black dependency and applied formatting

--- speed-daemon/app.py
+++ speed-daemon/app.py
@@ -19,7 +19,7 @@ def load_data():
 def parse_data(df):
     df["download_mbps"] = df["download"] / 1000000.0
     df["upload_mbps"] = df["upload"] / 1000000.0
-    df["timestamp"] = df["timestamp"].astype('datetime64[ns]')
+    df["timestamp"] = df["timestamp"].astype("datetime64[ns]")
     df["date"] = df.timestamp.dt.date
     df["day_of_week"] = df.timestamp.dt.day_name()
     df["hour_of_day"] = df.timestamp.dt.hour
@@ -27,7 +27,7 @@ def parse_data(df):
 
 
 def plot_timeseries(df):
-    fig, axs = plt.subplots(3,1)
+    fig, axs = plt.subplots(3, 1)
     fig.tight_layout(pad=1.0)
 
     axs[0].plot(df["timestamp"], df["download_mbps"], ".")
@@ -48,13 +48,12 @@ def plot_timeseries(df):
 
 def plot_histograms(df):
     bins = 25
-    fig, axs = plt.subplots(3,2)
+    fig, axs = plt.subplots(3, 2)
     fig.tight_layout(pad=1.0)
 
     axs[0][0].hist(df["download_mbps"], bins=bins)
     axs[0][0].set_xlabel("Mbps")
     axs[0][0].set_ylabel("Downloads")
-
 
     axs[1][0].hist(df["upload_mbps"], bins=bins)
     axs[1][0].set_xlabel("Mbps")
@@ -63,7 +62,6 @@ def plot_histograms(df):
     axs[2][0].hist(df["ping"], bins=bins)
     axs[2][0].set_xlabel("ms")
     axs[2][0].set_ylabel("Pings")
-
 
     axs[0][1].set_yscale("log")
     axs[0][1].hist(df["download_mbps"], bins=bins)
@@ -81,19 +79,19 @@ def plot_histograms(df):
 
 
 def plot_boxplot_set(download_data, upload_data, ping_data):
-    fig, axs = plt.subplots(3,1)
+    fig, axs = plt.subplots(3, 1)
     fig.tight_layout(pad=1.0)
 
     axs[0].set_title("Download Speed")
-    axs[0].boxplot(download_data, flierprops={"marker":"x"})
+    axs[0].boxplot(download_data, flierprops={"marker": "x"})
     axs[0].set_ylabel("Mbps")
 
     axs[1].set_title("Upload Speed")
-    axs[1].boxplot(upload_data, flierprops={"marker":"x"})
+    axs[1].boxplot(upload_data, flierprops={"marker": "x"})
     axs[1].set_ylabel("Mbps")
 
     axs[2].set_title("Ping")
-    axs[2].boxplot(ping_data, flierprops={"marker":"x"})
+    axs[2].boxplot(ping_data, flierprops={"marker": "x"})
     axs[2].set_ylabel("ms")
 
     return fig
@@ -117,17 +115,31 @@ def main():
 
     # Boxplot by Day of Week
     day_of_week_list = data.day_of_week.unique()
-    download_by_day_of_week = [data[data.day_of_week == day_name].download_mbps for day_name in day_of_week_list]
-    upload_by_day_of_week = [data[data.day_of_week == day_name].upload_mbps for day_name in day_of_week_list]
-    ping_by_day_of_week = [data[data.day_of_week == day_name].ping for day_name in day_of_week_list]
-    st.pyplot(plot_boxplot_set(download_by_day_of_week, upload_by_day_of_week, ping_by_day_of_week))
+    download_by_day_of_week = [
+        data[data.day_of_week == day_name].download_mbps
+        for day_name in day_of_week_list
+    ]
+    upload_by_day_of_week = [
+        data[data.day_of_week == day_name].upload_mbps for day_name in day_of_week_list
+    ]
+    ping_by_day_of_week = [
+        data[data.day_of_week == day_name].ping for day_name in day_of_week_list
+    ]
+    st.pyplot(
+        plot_boxplot_set(
+            download_by_day_of_week, upload_by_day_of_week, ping_by_day_of_week
+        )
+    )
 
     # Boxplot by Hour
     hour_list = data.hour_of_day.unique()
-    download_by_hour = [data[data.hour_of_day == hour].download_mbps for hour in hour_list]
+    download_by_hour = [
+        data[data.hour_of_day == hour].download_mbps for hour in hour_list
+    ]
     upload_by_hour = [data[data.hour_of_day == hour].upload_mbps for hour in hour_list]
     ping_by_hour = [data[data.hour_of_day == hour].ping for hour in hour_list]
     st.pyplot(plot_boxplot_set(download_by_hour, upload_by_hour, ping_by_hour))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
