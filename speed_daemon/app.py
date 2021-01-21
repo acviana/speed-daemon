d224441acc3eb52df6ef7131b6f7c973c423672b
Alex C. Viana <alexcostaviana@gmail.com>
2021-01-20 22:32:24 -0600

    Updated the parsing structure

--- speed_daemon/app.py
+++ speed_daemon/app.py
@@ -2,11 +2,13 @@ import datetime
 import glob
 import json
 from json import JSONDecodeError
+import time
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import streamlit as st
+
+from speed_daemon import visualize
 
 
 def load_data():
@@ -46,7 +48,6 @@ def parse_data(df, localization=None):
     df = df.set_index(a_index)
     if localization:
         df = df.set_index(df.index.tz_convert(localization))
-
     df = df.rename(columns={"timestamp": "_timestamp_string"})
 
     # Precompute datetime groupings
@@ -54,189 +55,6 @@ def parse_data(df, localization=None):
     df["day_of_week"] = df.index.day_name()
     df["hour_of_day"] = df.index.hour
     return df
-
-
-def plot_timeseries(df):
-    fig, axs = plt.subplots(3, 1)
-    fig.tight_layout(pad=1.0)
-
-    axs[0].plot(df["timestamp"], df["download_mbps"], ".")
-    axs[0].set_title("Download Speed")
-    axs[0].set_ylabel("Mbps")
-
-    axs[1].plot(df["timestamp"], df["upload_mbps"], ".")
-    axs[1].set_title("Upload Speed")
-    axs[1].set_ylabel("Mbps")
-
-    axs[2].plot(df["timestamp"], df["ping"], ".")
-    axs[2].set_title("Ping")
-    axs[2].set_xlabel("Date")
-    axs[2].set_ylabel("ms")
-
-    return fig
-
-
-def plot_histograms(df, stats):
-    bins = 25
-    fig, axs = plt.subplots(3, 2)
-    fig.tight_layout(pad=1.0)
-
-    plot_histogram(
-        ax=axs[0][0],
-        data=df["download_mbps"],
-        bins=bins,
-        mean=stats["download_mbps"]["mean"],
-        std=stats["download_mbps"]["std"],
-        y_label="Downloads",
-        x_label="Mbps",
-    )
-    plot_histogram(
-        ax=axs[1][0],
-        data=df["upload_mbps"],
-        bins=bins,
-        mean=stats["upload_mbps"]["mean"],
-        std=stats["upload_mbps"]["std"],
-        y_label="Upload",
-        x_label="Mbps",
-    )
-    plot_histogram(
-        ax=axs[2][0],
-        data=df["ping"],
-        bins=bins,
-        mean=stats["ping"]["mean"],
-        std=stats["ping"]["std"],
-        y_label="Ping",
-        x_label="ms",
-    )
-
-    plot_histogram(
-        ax=axs[0][1],
-        data=df["download_mbps"],
-        bins=bins,
-        mean=stats["download_mbps"]["mean"],
-        std=stats["download_mbps"]["std"],
-        x_label="Mbps",
-        y_scale="log",
-    )
-    plot_histogram(
-        ax=axs[1][1],
-        data=df["upload_mbps"],
-        bins=bins,
-        mean=stats["upload_mbps"]["mean"],
-        std=stats["upload_mbps"]["std"],
-        x_label="Mbps",
-        y_scale="log",
-    )
-    plot_histogram(
-        ax=axs[2][1],
-        data=df["ping"],
-        bins=bins,
-        mean=stats["ping"]["mean"],
-        std=stats["ping"]["std"],
-        x_label="ms",
-        y_scale="log",
-    )
-
-    return fig
-
-
-def plot_histogram(
-    ax,
-    data,
-    bins,
-    orientation="vertical",
-    mean=None,
-    std=None,
-    y_label=None,
-    x_label=None,
-    y_scale="linear",
-):
-    ax.hist(data, bins=bins, orientation=orientation)
-    if orientation == "vertical":
-        ax.axvline(mean - std, linestyle=":")
-        ax.axvline(mean, linestyle="--")
-        ax.axvline(mean + std, linestyle=":")
-    else:
-        ax.axhline(mean - std, linestyle=":")
-        ax.axhline(mean, linestyle="--")
-        ax.axhline(mean + std, linestyle=":")
-    ax.set_ylabel(y_label)
-    ax.set_xlabel(x_label)
-    ax.set_yscale(y_scale)
-
-
-def plot_scatterplot(ax, x_data, y_data, mean, std, title, y_label, x_label=None):
-    ax.plot(x_data, y_data, ".")
-    ax.axhline(mean - std, linestyle=":")
-    ax.axhline(mean, linestyle="--")
-    ax.axhline(mean + std, linestyle=":")
-    ax.set_title(title)
-    ax.set_ylabel(y_label)
-    ax.set_xlabel(x_label)
-
-
-def plot_summary(df, stats):
-    fig, axs = plt.subplots(3, 2)
-    fig.tight_layout(pad=1.0)
-
-    plot_scatterplot(
-        ax=axs[0][0],
-        x_data=df["timestamp"],
-        y_data=df["download_mbps"],
-        mean=stats["download_mbps"]["mean"],
-        std=stats["download_mbps"]["std"],
-        title="Download Speed",
-        y_label="Mbps",
-    )
-    plot_scatterplot(
-        ax=axs[1][0],
-        x_data=df["timestamp"],
-        y_data=df["upload_mbps"],
-        mean=stats["upload_mbps"]["mean"],
-        std=stats["upload_mbps"]["std"],
-        title="Upload Speed",
-        y_label="Mbps",
-    )
-    plot_scatterplot(
-        ax=axs[2][0],
-        x_data=df["timestamp"],
-        y_data=df["ping"],
-        mean=stats["ping"]["mean"],
-        std=stats["ping"]["std"],
-        title="Ping",
-        y_label="ms",
-        x_label="Date",
-    )
-
-    bins = 10
-    plot_histogram(
-        ax=axs[0][1],
-        data=df["download_mbps"],
-        bins=bins,
-        orientation="horizontal",
-        mean=stats["download_mbps"]["mean"],
-        std=stats["download_mbps"]["std"],
-    )
-    plot_histogram(
-        ax=axs[1][1],
-        data=df["upload_mbps"],
-        bins=bins,
-        orientation="horizontal",
-        mean=stats["upload_mbps"]["mean"],
-        std=stats["upload_mbps"]["std"],
-    )
-    plot_histogram(
-        ax=axs[2][1],
-        data=df["ping"],
-        bins=bins,
-        orientation="horizontal",
-        mean=stats["ping"]["mean"],
-        std=stats["ping"]["std"],
-    )
-
-    fig.autofmt_xdate()
-
-    return fig
 
 
 def get_summary_stats(df, days_of_week=False):
@@ -271,73 +89,69 @@ def get_summary_stats(df, days_of_week=False):
     return output
 
 
-def plot_mean_median_std(ax, data, data_type):
-    ax.errorbar(data.index, "mean", yerr="std", marker="o", linestyle="", data=data)
-    ax.plot(data.index, "median", marker="x", linestyle="", data=data)
-    ax.set_title(data_type)
-    if data_type in ["Download", "Upload"]:
-        ax.set_ylabel("Mbps")
-    elif data_type == "Ping":
-        ax.set_ylabel("ms")
-
-
-def plot_timeseries_summary(df):
-    fig, axs = plt.subplots(3, 1)
-    fig.tight_layout(pad=1.0)
-
-    plot_mean_median_std(axs[0], df["download_mbps"], "Download")
-    plot_mean_median_std(axs[1], df["upload_mbps"], "Upload")
-    plot_mean_median_std(axs[2], df["ping"], "Ping")
-
-    fig.autofmt_xdate()
-
-    return fig
-
-
 def main():
+    load_and_parse_t1 = time.time()
     data = load_data()
-    data = parse_data(data)
+    # TODO: Made the localization a environment variable
+    data = parse_data(data, localization="US/Central")
+    load_and_parse_t2 = time.time()
+    load_and_parse_time = load_and_parse_t2 - load_and_parse_t1
     sns.set_theme()
 
     # Scale of Data
     date_list = data.date.unique()
-    st.text(f"Analyzing {len(data)} data points over {len(date_list)} days")
+    st.text(f"Analyzing {len(data):,} data points over {len(date_list)} days")
+    st.text(f"Loaded {len(data):,} data points in {load_and_parse_time:0.2f}s")
 
     # Last Reading
     st.subheader("Last Reading")
-    st.table(data.iloc[0][["timestamp", "download_mbps", "upload_mbps", "ping"]])
+    st.table(
+        data[data.index == data.index.max()][["download_mbps", "upload_mbps", "ping"]]
+    )
 
     # Today's Stats
     st.subheader("Last 24 Hours")
-    today = data[data.timestamp.dt.date == datetime.datetime.today().date()]
+    # TODO: This is not right, do we want last 24 hours or today's data?
+    today = data[data.date == datetime.datetime.today().date()]
     today_stats = get_summary_stats(today)
     st.table(today_stats)
-    st.pyplot(plot_summary(df=today, stats=today_stats))
+    st.pyplot(visualize.plot_summary(df=today, stats=today_stats))
 
     # Overall Stats
     st.subheader("All Data")
     overall_stats = get_summary_stats(data)
     st.table(overall_stats)
-    st.pyplot(plot_summary(df=data, stats=overall_stats))
-    st.pyplot(plot_histograms(df=data, stats=overall_stats))
+    st.text(dir(data.index.values))
+    st.pyplot(visualize.plot_summary(df=data, stats=overall_stats))
+    st.pyplot(visualize.plot_histograms(df=data, stats=overall_stats))
 
-    stats_by_date = get_summary_stats(data.groupby(data.timestamp.dt.date))
+    stats_by_date = get_summary_stats(data.groupby(data.index.values))
 
     # Boxplot Timeseries
     st.subheader("Data by Date")
-    st.pyplot(plot_timeseries_summary(stats_by_date))
+    st.pyplot(visualize.plot_timeseries_summary(stats_by_date))
 
     # Boxplot by Day of Week
     st.subheader("Data by Day of Week")
     stats_by_day_of_week = get_summary_stats(
         data.groupby(data.day_of_week), days_of_week=True
     )
-    st.pyplot(plot_timeseries_summary(stats_by_day_of_week))
+    st.pyplot(visualize.plot_timeseries_summary(stats_by_day_of_week))
 
     # Boxplot by Hour
     st.subheader("Data by Hour")
     stats_by_hour_of_day = get_summary_stats(data.groupby(data.hour_of_day))
-    st.pyplot(plot_timeseries_summary(stats_by_hour_of_day))
+    st.pyplot(visualize.plot_timeseries_summary(stats_by_hour_of_day))
+
+    # Weekday Boxplot by Hour
+    st.subheader("Weekday Data by Hour")
+    # TODO: Move this to a parsing function. Add is_weekday field.
+    weekday_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    weekday_data = data[data.day_of_week.isin(weekday_list)]
+    weekdat_stats_by_hour_of_day = get_summary_stats(
+        data.groupby(weekday_data.hour_of_day)
+    )
+    st.pyplot(visualize.plot_timeseries_summary(weekdat_stats_by_hour_of_day))
 
 
 if __name__ == "__main__":
